8eec6fd370c76c89d8b541049cd2d512255425d5
Renan Leonel <62075475+renan-leonel@users.noreply.github.com>
2022-09-03 16:58:31 -0300

    Time: 596 ms (69.99%), Space: 15.6 MB (24.05%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -2,23 +2,21 @@ import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        maximo = max(piles)
-        hours = 0
-        minimoPossivel, maximoImpossivel = 0, 0
+        left, right = 1, max(piles)
+        local = right
 
-        while True:
-            for i in piles:
-                hours += math.ceil(i/maximo)
-
-            if hours <= h:
-                minimoPossivel = maximo
-                maximo = (maximo + maximoImpossivel)//2
-
-            else:
-                maximoImpossivel = maximo
-                maximo = (maximo + minimoPossivel)//2 
-
-            if minimoPossivel == maximoImpossivel+1:
-                return minimoPossivel
-
+        while left <= right:
+            speed = (left+right)//2
             hours = 0
+
+            for i in piles:
+                hours += math.ceil(i/speed)
+
+            if hours > h:
+                left = speed + 1
+                
+            else:
+                local = min(local, speed)
+                right = speed - 1
+
+        return local
